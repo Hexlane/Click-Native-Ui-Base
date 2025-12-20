c87999768508333450089a1d5753d7d385ae2f4b
lootte <deede3771@gmail.com>
2025-12-20 05:15:59 -0500

    Update Lists.h

--- BigBaseV2/src/gui/list/Lists.h
+++ BigBaseV2/src/gui/list/Lists.h
@@ -1,1 +1,30 @@
 
+#pragma once
+#include "UI/UIManager.hpp"
+
+namespace big::Lists
+{
+	inline const char* HeaderTypesFrontend[]
+	{
+		"Static",
+	};
+	inline UserInterface::HeaderType HeaderTypesBackend[]
+	{
+		UserInterface::HeaderType::Static,
+	};
+	inline std::size_t HeaderTypesPosition = 1;
+	inline const char* DemoList[10]
+	{
+		"One",
+		"Two",
+		"Three",
+		"Four",
+		"Five",
+		"Six",
+		"Seven",
+		"Eight",
+		"Nine",
+		"Ten"
+	};
+	inline std::size_t DemoListPos = 0;
+}
