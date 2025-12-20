0a27725b6c596c9e33251a717f7ef28900b0e8a5
lootte <deede3771@gmail.com>
2025-12-20 05:17:55 -0500

    Update submenu.hpp

--- BigBaseV2/src/gui/list/submenu.hpp
+++ BigBaseV2/src/gui/list/submenu.hpp
@@ -4,34 +4,6 @@ namespace big {
 	enum Submenu : std::uint32_t
 	{
 		SubmenuHome,
-
-		SubmenuSelf,
-		SubmenuSelfAnimation,
-		SubmenuSelfGlow,
-
-		SubmenuPlayers,
-		SubmenuSelectedPlayer,
-		SubmenuPlayerFriendly,
-		SubmenuPlayerEvil,
-		SubmenuPlayerVehicle,
-
-		SubmenuNetwork,
-
-		SubmenuVehicle,
-
-		SubmenuWeapon,
-
-
-		SubmenuRecovery,
-
-		SubmenuTeleports,
-
-		SubmenuProtection,
-
-		SubmenuWorld,
-
-		SubmenuSettings,
-
-		SubmenuOptions
+		SubmenuDemo,
 	};
 }
